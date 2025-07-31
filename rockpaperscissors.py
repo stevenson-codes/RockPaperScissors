740a31cc57c42b37732ee16b9989fa142bce45cd
your-username <your-email>
2025-07-31 15:38:56 -0700

    Added basic upgrades

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,14 +2,19 @@ import random
 
 cpu = random.choice(['rock', 'paper', 'scissors'])
 user = input('Rock, Paper or Scissors?\n').lower()
+wins, ties, loses = 0, 0, 0
 
-if cpu == user:
-    print('TIE')
-elif cpu == 'rock' and user == 'paper':
-    print('WIN')
-elif cpu == 'paper' and user == 'scissors':
-    print('WIN')
-elif cpu == 'scissors' and user == 'rock':
-    print('WIN')
-else:
-    print('LOSE')
+while user != 'exit':
+    if cpu == user:
+        print('TIE')
+        ties += 1
+    elif (cpu == 'rock' and user == 'paper' or
+        cpu == 'paper' and user == 'scissors' or
+        cpu == 'scissors' and user == 'rock'):
+        wins += 1
+        print('WIN')
+    else:
+        print('LOSE')
+        loses += 1
+    print('Wins:', wins, 'Loses:', loses, 'Ties:', ties, '\n')
+    user = input('Rock, Paper or Scissors?\n').lower()
